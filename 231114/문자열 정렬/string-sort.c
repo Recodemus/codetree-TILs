427c0389d2616fefd60eb7fd27765ad1861e4184
Recodemus <145319715+Recodemus@users.noreply.github.com>
2023-11-14 17:53:19 +0900

    Title: 문자열 정렬, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231114/문자열 정렬/string-sort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+void sort(char a[], int len);
+
+void swap(char *n1, char *n2);
+
+int main() {
+    char arr[100];
+    int i, j, n;
+
+    scanf("%d", &n);
+
+    scanf("%s", arr);
+
+    n = strlen(arr);
+
+    sort(arr, n);
+
+    for(i = n - 1;i >= 0;i--)
+    {
+        printf("%c", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+void sort(char a[], int len)
+{
+    int i, j;
+
+    for(i = 0;i < len-1;i++)
+    {
+        for(j = 0;j < len;j++)
+        {
+            if(a[i] > a[j])
+            {
+                swap(&a[i], &a[j]);
+            }
+        }
+    }
+}
+
+void swap(char *n1, char *n2)
+{
+    char temp;
+
+    temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
