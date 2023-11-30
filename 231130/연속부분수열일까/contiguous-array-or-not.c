28ee3acfac729d03e226c25cfc39cb5401b93a8d
Recodemus <145319715+Recodemus@users.noreply.github.com>
2023-11-30 21:36:56 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -1,81 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void input(int len, int* arr)
-{
-    int i;  //for문
-
-    for (i = 0; i < len; i++)  //배열 입력
-    {
-        scanf("%d", &arr[i]);
-    }
-}
-
-int fun(int len1, int len2, int* p1, int* p2)
-{
-    int i, j = 0;    //for문
-    int start;   //시작
-
-    for (i = 0; i <= len1; i++)
-    {
-        if (p1[i] == p2[0])
-        {
-            start = i;
-            break;
-        }
-
-        if (i == len1)
-        {
-            return 0;
-        }
-
-        else
-        {
-            continue;
-        }
-    }
-
-    for (i = start; i < len1; i++)
-    {
-        if (p1[start + 1] != p2[1])
-        {
-            return 0;
-        }
-
-        else
-        {
-            return 1;
-        }
-    }
-}
-
-int main()
-{
-    int n1, n2 , sw; //두 수열의 원소 개수
-    int* a, * b;   //수열
-
-    scanf("%d %d", &n1, &n2);  //원소 개수 입력
-
-    a = (int*)malloc(sizeof(int) * n1);    //수열 a
-    b = (int*)malloc(sizeof(int) * n2);    //수열 b
-
-    input(n1, a);
-    input(n2, b);
-
-    sw = fun(n1, n2, a, b);
-
-    if (sw == 1)
-    {
-        printf("Yes");
-    }
-
-    else
-    {
-        printf("No");
-    }
-
-    free(a);
-    free(b);
-
+int main() {
+    // 정수 n1과 n2를 입력받습니다.
+	int n1, n2;
+	scanf("%d %d", &n1, &n2);
+	int arr1[100];
+	int arr2[100];
+	
+	// arr1을 입력받습니다.
+	for(int i = 0; i < n1; i++)
+		scanf("%d", &arr1[i]);
+	
+	// arr2를 입력받습니다.
+	for(int i = 0; i < n2; i++)
+		scanf("%d", &arr2[i]);
+	
+	// arr2이 arr1의 연속부분수열인지 확인합니다.
+	for(int i = 0; i < n1; i++) {
+		// arr1의 i번 index부터의 수열이 arr2와 완전히 일치하는지 확인합니다.
+		// 즉, arr1[i] == arr2[0], arr1[i+1] == arr2[1]...이 성립하는지 확인합니다.
+		// success : arr1의 i번 index부터의 수열이 arr2와 완전히 일치할때만 true, 그 외 false
+		bool success = true;
+		
+		for(int j = 0; j < n2; j++) {
+			// arr1의 index가 범위 밖으로 벗어날때
+			if(i + j >= n1) {
+				success = false;
+				break;
+			}
+			
+			// arr1과 arr2가 일치하지 않을때
+			if(arr1[i + j] != arr2[j]) {
+				success = false;
+				break;
+			}
+		}
+		
+		// 완전히 일치할 경우, arr2는 arr1의 연속부분수열이 맞습니다.
+		// 구현의 편의를 위해 return 0으로 프로그램을 강제 종료하였습니다.
+		if(success) {
+			printf("Yes");
+			return 0;
+		}
+	}
+	
+	// 완전히 일치하는 경우가 하나도 없을 경우, arr2는 arr1의 연속부분수열이 아닙니다.
+	printf("No");
+	
     return 0;
 }
