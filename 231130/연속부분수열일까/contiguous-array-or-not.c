1b528b6d44ac4dc7cf7dd6ccc976588a66770f39
Recodemus <145319715+Recodemus@users.noreply.github.com>
2023-11-30 20:55:01 +0900

    Title: 연속부분수열일까, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -1,70 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void input(int len, int *arr)
+void input(int len, int* arr)
 {
     int i;  //for문
 
-    for(i = 0;i < len;i++)  //배열 입력
+    for (i = 0; i < len; i++)  //배열 입력
     {
-        scanf("%d ", &arr[i]);
+        scanf("%d", &arr[i]);
     }
 }
 
-int fun(int len1, int len2, int *p1, int *p2)
+int fun(int len1, int len2, int* p1, int* p2)
 {
     int i, j = 0;    //for문
     int start;   //시작
 
-    for(i = 0;i < len1;i++)
+    for (i = 0; i < len1; i++)
     {
-        if(p1[i] == p2[0])
+        if (p1[i] == p2[0])
         {
             start = i;
             break;
         }
+    }
 
-        else
+    for (i = start; i < len1; i++)
+    {
+        if (p1[start + 1] != p2[1])
         {
-            printf("No");
             return 0;
         }
     }
-
-    for(i = start;i < len1;i++)
-        {
-            if(i == start)
-            {
-                continue;
-            }
-
-            if(p1[start + 1] == p2[1])
-            {
-                printf("Yes");
-            }
-
-            else
-            {
-                printf("No");
-                return 0;
-            }
-        }
+    return 1;
 }
 
 int main()
 {
-    int n1, n2; //두 수열의 원소 개수
-    int *a, *b;   //수열
+    int n1, n2 , sw; //두 수열의 원소 개수
+    int* a, * b;   //수열
 
-    scanf("%d %d",&n1, &n2);  //원소 개수 입력
+    scanf("%d %d", &n1, &n2);  //원소 개수 입력
 
-    a = (int *)malloc(sizeof(int) * n1);    //수열 a
-    b = (int *)malloc(sizeof(int) * n2);    //수열 b
+    a = (int*)malloc(sizeof(int) * n1);    //수열 a
+    b = (int*)malloc(sizeof(int) * n2);    //수열 b
 
     input(n1, a);
     input(n2, b);
 
-    fun(n1, n2, a, b);
+    sw = fun(n1, n2, a, b);
+
+    if (sw == 1)
+    {
+        printf("Yes");
+    }
+
+    else
+    {
+        printf("No");
+    }
 
     free(a);
     free(b);
